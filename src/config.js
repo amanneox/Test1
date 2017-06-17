3e974fe1e25916916f0ea06e958411b2403518dc
amanneox <amanadhikari2@gmail.com>
2017-06-17 19:44:50 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,11 +1,3 @@
-const config = {
-
-
-  token: 'de65a8bad21da16cec79c79b90f50b17',
-  devtoken: 'af6d5da405f5632669bf990e5d0687f2',
-  slug: 'amanneox',
-}
-
-module.exports = config
-
-const REQUEST_TOKEN = require('token')
+process.env.PORT = '5000'
+process.env.REQUEST_TOKEN = ‘de65a8bad21da16cec79c79b90f50b17’
+process.env.LANGUAGE = 'en'
